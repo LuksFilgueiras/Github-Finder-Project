e3a85a07ea2cf949a75639a9161b7aec6359ca88
Lucas Filgueiras <lucasfilgueiras@rocketmail.com>
2022-07-13 16:49:53 -0300

    User stats + Get user repos

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useReducer } from "react";
+import { createRenderer } from "react-dom/test-utils";
 import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext()
@@ -10,6 +11,7 @@ export const GithubProvider = ({children}) => {
     const initialState = {
         users: [],
         user: {},
+        repos: [],
         loading: false
     }
 
@@ -62,6 +64,28 @@ export const GithubProvider = ({children}) => {
         }
     }
 
+    //Get user repos
+    const getUserRepos = async (login) =>{
+        setLoading()
+
+        const params = new URLSearchParams({
+            sort: 'created',
+            per_page: 10,
+        })
+        const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`,
+        { 
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`,
+            }
+        })
+        const data = await response.json()
+        dispatch({
+            type: 'GET_USER_REPOS',
+            payload: data,
+        })
+    }
+
+
 
     const setLoading = () => dispatch({
         type: 'SET_LOADING',
@@ -71,9 +95,11 @@ export const GithubProvider = ({children}) => {
         users: state.users,
         loading: state.loading,
         user: state.user,
+        repos: state.repos,
         searchUsers,
         clearUsers,
         getUser,
+        getUserRepos,
     }}>
 
         {children}
